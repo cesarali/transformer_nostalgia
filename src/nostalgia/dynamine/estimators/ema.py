a6fff506f47a30499adea0587967807d2f27c0f5
cesarali <cesarali07@gmail.com>
2023-10-31 11:37:35 +0100

    gitignore

---
+++ src/nostalgia/dynamine/estimators/ema.py
@@ -0,0 +1,35 @@
+import math
+import torch
+EPS = 1e-6
+
+class EMALoss(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input, running_ema):
+        ctx.save_for_backward(input, running_ema)
+        input_log_sum_exp = input.exp().mean().log()
+
+        return input_log_sum_exp
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, running_mean = ctx.saved_tensors
+        grad = grad_output * input.exp().detach() / \
+            (running_mean + EPS) / input.shape[0]
+        return grad, None
+
+
+def ema(mu, alpha, past_ema):
+    return alpha * mu + (1.0 - alpha) * past_ema
+
+
+def ema_loss(x, running_mean, alpha):
+    t_exp = torch.exp(torch.logsumexp(x, 0) - math.log(x.shape[0])).detach()
+    if running_mean == 0:
+        running_mean = t_exp
+    else:
+        running_mean = ema(t_exp, alpha, running_mean.item())
+    t_log = EMALoss.apply(x, running_mean)
+
+    # Recalculate ema
+
+    return t_log, running_mean
